4947a6eeec8bea69be0b7285f6faaa638c56ff09
Anderson Sarmento <anderson.sarmento@gmail.com>
2023-11-30 22:11:12 -0300

    Quinta feira mesmo!

--- Desafios Mundo 2/ex061.py
+++ Desafios Mundo 2/ex061.py
@@ -4,17 +4,11 @@ print('')
 print('Refaça o DESAFIO 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão usando a estrutura while.')
 print('')
 
-
-
 primeiro = int(input('Primeiro termo: '))
 razão = int(input('Razão: '))
-décimo = primeiro + (10-1)* razão
-print('Primeiro Termo:',0)
-print('Razão:',2)
-for c in range(primeiro,décimo+razão,razão):
-    print(c, end='-')   
-
+contador = 1
+while contador != 10:
+    print(primeiro + (contador)* razão,end='-')
+    contador =contador+1
 print('Acabou')
 print(40*'=')
-
-while 
