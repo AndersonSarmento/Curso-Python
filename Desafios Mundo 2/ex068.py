96d78543d8eff6259a7f5ed314f42653b7a924e2
Anderson Sarmento <anderson.sarmento@gmail.com>
2023-12-10 17:09:01 -0300

    Exercício Python 067

---
+++ Desafios Mundo 2/ex068.py
@@ -0,0 +1,7 @@
+print()
+print('{:=^80}'.format('Exercício Python 068'))
+print('')
+print('Faça um programa que jogue par ou ímpar com o computador.\n O jogo só será interrompido quando o jogador perder, mostrando o total de vitórias consecutivas que ele conquistou no final do jogo. ')
+print()
+
+
