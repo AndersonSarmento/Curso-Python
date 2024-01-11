aa474c302130699b0a45f1d38853db4440e4b5fb
Anderson Sarmento <anderson.sarmento@gmail.com>
2024-01-10 22:33:04 -0300

    Tabela

--- Desafios Mundo 2/ex073.py
+++ Desafios Mundo 2/ex073.py
@@ -1,1 +1,8 @@
 print()
+print()
+
+
+tabela =('Palmeiras', 'Flamengo', 'Internacional', 'Gremio', 'Sao Paulo',
+         'Atletico-MG', 'Athletico-PR','Cruzeiro','Botafogo','Santos',
+         'Bahia','Fluminense','Corinthians','Chapecoense','Ceara','Vasco'
+         ,'América-MG' ,'Sport', 'Vitória','Parana')
