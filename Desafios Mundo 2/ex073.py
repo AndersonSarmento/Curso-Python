37fa3780a81d4dfe30f5c57f7dd6ae20e6808ead
Anderson Sarmento <anderson.sarmento@gmail.com>
2024-01-10 22:42:20 -0300

    One more

--- Desafios Mundo 2/ex073.py
+++ Desafios Mundo 2/ex073.py
@@ -1,8 +1,17 @@
-print()
-print()
+print('')
+print('{:=^105}'.format('Exercício Python 073'))
+print('')
 
+print('Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol,\nna ordem de colocação. Depois mostre:')
+print(' a) Os 5 primeiros times.')
+print(' b) Os últimos 4 colocados.')
+print(' c) Times em ordem alfabética.') 
+print(' d) Em que posição está o time da Chapecoense.') 
+print('')
 
 tabela =('Palmeiras', 'Flamengo', 'Internacional', 'Gremio', 'Sao Paulo',
          'Atletico-MG', 'Athletico-PR','Cruzeiro','Botafogo','Santos',
          'Bahia','Fluminense','Corinthians','Chapecoense','Ceara','Vasco'
          ,'América-MG' ,'Sport', 'Vitória','Parana')
+
+
