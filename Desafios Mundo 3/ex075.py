5905abcb8bc5fe824ea41cb7b1aa7333bafecfcb
Anderson Sarmento <anderson.sarmento@gmail.com>
2024-01-19 23:19:33 -0300

    ex075

---
+++ Desafios Mundo 3/ex075.py
@@ -0,0 +1,8 @@
+print()
+print('{:=^80}'.format(' Exercício Python 75 '))
+print('Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla.')
+print('No final, mostre:')
+print('A) Quantas vezes apareceu o valor 9.')
+print('B) Em que posição foi digitado o primeiro valor 3.')
+print('C) Quais foram os números pares.')
+print()
