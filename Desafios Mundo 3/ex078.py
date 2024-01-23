39aecbf5e8c6979fe461b275e0aebf2443419ab1
Anderson Sarmento <anderson.sarmento@gmail.com>
2024-01-22 23:43:46 -0300

    ex078.py

--- Desafios Mundo 3/ex078.py
+++ Desafios Mundo 3/ex078.py
@@ -24,7 +24,33 @@ for pos,i in enumerate(lista):
         print(f'O maior valor digitado foi {maior} nas posições {pos}')
     elif menor == i:
         print(f'O menor valor digitado foi {menor} nas posições {pos}')
+        
+print('---------------------------------------------------------------------------------------')
+print('{:=^80}'.format(' Solução Guanabara'))
 
+listanum=[]
+mai = men = 0
+for c in range(0,5):
+    listanum.append(int(input(f'Digite um valor para a posição{c}: ')))
+    if c == 0:
+        mai = men = listanum[c]
+    else:
+        if listanum[c] > mai:
+            mai = listanum[c]
+        if listanum[c] < men:
+            men = listanum[c]
+
+print(f'Você digitou os valores {listanum}')
+print(f'O maior valor digitado foi {mai} nas posicões ', end='')
+for i, v in enumerate(listanum):
+    if v == mai:
+        print(f'{i}...', end='')
+print('')
+print(f'O menor valor digitado foi {men} nas posicões ', end='')
+for i, v in enumerate(listanum):
+    if v == men:
+        print(f'{i}...', end='')
+print('')
 
 
 
