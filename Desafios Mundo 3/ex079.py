9fe21b7ffd0df848fd0186f5841bb3f3cc5edaa7
Anderson Sarmento <anderson.sarmento@gmail.com>
2024-02-01 23:42:34 -0300

    ex079.py

---
+++ Desafios Mundo 3/ex079.py
@@ -0,0 +1,7 @@
+print('')
+print('{:=^80}'.format(' Exercício Python 79 '))
+print('')
+print('Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista.')
+print('Caso o número já exista lá dentro, ele não será adicionado.')
+print('No final, serão exibidos todos os valores únicos digitados, em ordem crescente')
+print('')
