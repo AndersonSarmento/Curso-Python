d53812b890098f16d5b661bb88ecd6ebe1de4b48
Anderson Sarmento <anderson.sarmento@gmail.com>
2024-02-01 23:53:23 -0300

    ex079.py

--- Desafios Mundo 3/ex079.py
+++ Desafios Mundo 3/ex079.py
@@ -5,3 +5,12 @@ print('Crie um programa onde o usuário possa digitar vários valores numéricos
 print('Caso o número já exista lá dentro, ele não será adicionado.')
 print('No final, serão exibidos todos os valores únicos digitados, em ordem crescente')
 print('')
+valores = []
+
+numero = int(input('Digite o seu número'))
+valores.append(numero)
+print(valores)
+
+
+
+
